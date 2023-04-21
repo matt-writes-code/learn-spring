47689a1c440fe3526f0a67875fd7717a3ba45432
mayojich <mayojich@gmail.com>
2023-04-21 23:02:04 +0800

    learn-spring

---
+++ src/main/java/com/amigoscode/CourseController.java
@@ -0,0 +1,19 @@
+package com.amigoscode;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1, "AWS","Awesome Matt"),
+                new Course(2, "Azure", "AwesomeMatt")
+        );
+    }
+}
